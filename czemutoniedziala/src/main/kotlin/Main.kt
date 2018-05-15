889a2dbfe3511898850b0ddd5493e3bc90dc8970
aaaggaa <kwiecinskagnieszk.a@gmail.com>
2018-05-15 18:38:16 +0200

    Project init

---
+++ czemutoniedziala/src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    printFizzBuzz(start = 0, end = 100)
+}
+
+fun printFizzBuzz(start: Int, end: Int) {
+    for (i in start..end) {
+        if (i % 15 == 0)
+            println("$i FizzBuzz")
+         else
+            if (i % 5 == 0)
+                println("$i Buzz")
+            else
+                if (i % 3 == 0)
+                    println("$i Fizz")
+                            else
+        println("$i")
+    }
+        }
+
+
+
+
