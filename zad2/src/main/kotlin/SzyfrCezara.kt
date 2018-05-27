5adf24cf1c93f193bffa3b51ec1de3e479ff8733
aaaggaa <kwiecinskagnieszk.a@gmail.com>
2018-05-27 08:40:47 +0200

    Zad2 implementation

--- zad2/src/main/kotlin/SzyfrCezara.kt
+++ zad2/src/main/kotlin/SzyfrCezara.kt
@@ -1,21 +1,21 @@
-fun main(strin: Array<String>) {
+fun main (string: Array<String>) {
     val encrypted = """fgbv an fgnpwv ybxbzbgljn,
-                        pvężxn, btebzan v cbg m avrw fcłljn:
-                        głhfgn byvjn.
-                        fgbv v fncvr, qlfml v qzhpun,
-                        żne m ebmtemnartb wrw oemhpun ohpun:
-                        ohpu - wnx tbeąpb!
-                        hpu - wnx tbeąpb!
-                        chss - wnx tbeąpb!
-                        hss - wnx tbeąpb!
-                        whż yrqjb fncvr, whż yrqjb mvcvr,
-                        n wrfmpmr cnynpm jętvry j avą flcvr.""".trimMargin()
+                       pvrmxn, btebzan v cbg m avrw fcyljn:
+                       gyhfgn byvjn.
+                       fgbv v fncvr, qlfml v qzhpun,
+                       mne m ebmtemnartb wrw oemhpun ohpun:
+                       ohpu - wnx tbenpb!
+                       hpu - wnx tbenpb!
+                       chss - wnx tbenpb!
+                       hss - wnx tbenpb!
+                       whm yrqjb fncvr, whm yrqjb mvcvr,
+                       n wrfmpmr cnynpm jrtvry j avn flcvr.""".trimMargin()
 
-    val decrypted = decrypt(encrypted,13)
-
+    val decrypted = decryptCaesar(encrypted, 13)
+for (i in 0 until decrypted.length)
     println(decrypted)
 }
 
-fun decrypt(encrypted : String, key : Int) : String{
+fun decryptCaesar(encrypted : String, key : Int) : String{
     TODO()
 }
--- zad2/src/main/kotlin/Zad2.kt
+++ zad2/src/main/kotlin/Zad2.kt
@@ -1,12 +1,51 @@
 fun main(args: Array<String>) {
     val source = "abcdefghijklmnoprstuwvxyz"
+    val reversed = revertString(source)
 
-   val reverted =  revertString(source)
+    println(reversed)
+    for (i in source.count() -1 downTo 0 ){
 
-    println(reverted)
-    //output should be zyxvxwutsrpomn...
+        println (source.get(i))
+
+    }
+    val tojestint: Long = 555555555555555
+    var tojeststring: String = "a"
+    var tojestlong = 8L
+    var cokolwiek: Byte = 122
+    val ujemna = -14
+    val kkk: Double = 1.111
+    val sss: Char = 'a'
+
+    val ddd: Array<Double> = arrayOf(2.0, 5.6)
+    val dlugosc = ddd.size
+    val list: List<String> = listOf("java", "kotlin", "C#", "Lisp", tojeststring)
+    val size: Int = list.size
+    val zbior: Set<String> = setOf("a","b","c","d")
+//    println(ddd.get(1))
+//    println(list.get(2))
+//    println(zbior.get(2))
+    val kot: String = "Agnieszka"
+    val kot2 : String = "makota"
+    val liczbaznakow: Int = kot.length
+//    for (i in kot.count() -1 downTo 0 ){
+
+//        println (kot.get(i))
+
+//    }
+//    for (znak in kot){
+//        println(znak)
+//    }
+//    println(kot.length)
+//    println(kot2 + kot)
+//    println(kot.get(0))
+//    println(kot.get(3))
+//    println(kot.get(1))
+//    println(kot.get(2))
+
 }
 
-fun revertString(source: String): String {
-    TODO("not implemented")
+fun revertString(agnieszka: String): String {
+    return agnieszka
 }
+
+//fun stringtoin (lunge: Array<String>):
